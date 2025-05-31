9489c33d67a4d42d614f65613c77bb84f6ca7bc3
ibrahim2818 <ibrahim0001@std.bdu.ac.bd>
2025-05-31 21:58:58 +0600

    login view added

--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .utils import user_directory_path
 
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128)
+    address_2 = models.CharField(max_length=128 , blank=True)
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=5)
 class Profile(models.Model):
     user= models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField( null=True)
+    photo = models.ImageField( upload_to=user_directory_path, null=True)
     bio= models.CharField(max_length=140, blank=True)
     phone_number= models.CharField(max_length=12, blank=True)
+    location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.user.username}\'s profile'
 
+        # This method is called when you use the print() function or str() function on an object.
+        # Here, it returns a formatted string using an f-string.
+        # It accesses the 'user' attribute of the current 'Profile' object, which is expected to be a 'User' instance.
+        # Then, it retrieves the 'username' attribute of the 'User' object, which is a string representing the user's username.
+        # Finally, it returns a string in the format "<username>'s profile".
+        #return f"{self.user.username}'s profile"
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def login_view(request):
+    login_form = AuthenticationForm()
+    return render(request, 'login.html',{'login_form': login_form})
